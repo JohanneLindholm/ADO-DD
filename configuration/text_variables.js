1b9663f4f6290e0c2ccce849a766910f6bc6f3d1
JohanneLindholm <jal.marktg@cbs.dk>
2025-01-22 17:15:43 +0100

    Update text_variables.js

--- configuration/text_variables.js
+++ configuration/text_variables.js
@@ -1,1 +1,6 @@
+const textVariables = {
+    welcomeMessage: "Welcome to the experiment. Press any key to begin.",
+    instructions: "You will choose between smaller-sooner and larger-later options.",
+    thankYouMessage: "Thank you for participating!",
+};
 
